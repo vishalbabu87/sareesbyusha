61af87d6c0c6e12587a65805bd36536acbff21c1
vishal <vishal@example.com>
2026-04-11 00:18:32 +0530

    chore: use modern serverExternalPackages key

--- next.config.mjs
+++ next.config.mjs
@@ -11,9 +11,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
+  serverExternalPackages: ['@prisma/client'],
 };
 
 export default nextConfig;
